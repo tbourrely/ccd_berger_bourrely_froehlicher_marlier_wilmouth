8208e7bfa8d4a6c5b5214e8bf81dc63aa313603c
Axel Froehlicher <axel.froehlicher4@etu.univ-lorraine.fr>
2017-02-09 11:18:05 +0100

    ajout list Users

--- src/controllers/UtilisateursController.php
+++ src/controllers/UtilisateursController.php
@@ -2,10 +2,16 @@
 
 namespace charly\controllers;
 
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class UtilisateursController extends BaseController
 {
 
-    
+    public function listUsers(RequestInterface $req, ResponseInterface $resp, $args){
+        $tab['users'] = \charly\models\User::all();
+
+        $this->render($resp, 'utilisateurs/listUtilisateurs',$tab);
+    }
 
 }
