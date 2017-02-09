24b8aa72810529552e03c9d324b80f88e8402c5b
Axel Froehlicher <axel.froehlicher4@etu.univ-lorraine.fr>
2017-02-09 10:08:55 +0100

    Ajout Model

---
+++ src/models/user.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace charly\models;
+
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 09/02/2017
+ * Time: 10:03
+ */
+
+use Illuminate\Database\Eloquent\Model;
+
+class user extends Model
+{
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
