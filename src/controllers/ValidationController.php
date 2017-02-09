aca2d1a773f816f8797c6ab1e41292ca07244437
Pierre Berger <pierre.berger5@etu.univ-lorraine.fr>
2017-02-09 18:31:47 +0100

    Ajout valider

--- src/controllers/ValidationController.php
+++ src/controllers/ValidationController.php
@@ -12,14 +12,17 @@ namespace charly\controllers;
 use charly\models\Invitation;
 use charly\models\Logement;
 use Slim\Interfaces\InvocationStrategyInterface;
+use charly\models\Groupe;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class ValidationController extends BaseController
 {
     public function validerGroupe(RequestInterface $request, ResponseInterface $response, $args){
-        if(isset($_SESSION['user']['id]'])) {
-             $g = Groupe::where('proprietaire', $_SESSION['user']['id'])->where('id', $args['id'])->first();
+        if(isset($_SESSION['user']['id'])) {
+             $g = Groupe::where('proprietaire', $_SESSION['user']['id'])->where('id', $request->getParam('validate'))->first();
             if(!is_null($g)){
-                $l=Logement::where('id',$g->idLogement);
+                $l=Logement::where('id', 1)->first();
                 if(isset($l)){
                     if($l->places==$g->nbUsers){
                         $g->status='complet';
@@ -27,6 +30,7 @@ class ValidationController extends BaseController
                         $this->flash('info', 'Le groupe a bien été accepté');
                         return $this->redirect($response, 'viewGroup', $args);
                     }else{
+
                         $this->flash('info', 'Le nombre de places du logement ne corresponds pas au nombre de membre du groupe.');
                         return $this->redirect($response, 'viewGroup', $args);
                     }
