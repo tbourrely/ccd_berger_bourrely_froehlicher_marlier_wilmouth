54490fad8bcc5e28fe7712fd0e3a3ba63c50377d
Axel Froehlicher <axel.froehlicher4@etu.univ-lorraine.fr>
2017-02-09 21:17:20 +0100

    enlever bouton supprimer

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 09/02/2017
+ * Time: 21:00
+ */
+
+namespace charly\controllers;
+
+
+use charly\models\Logement;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class HomeController extends BaseController
+{
+    public function index(RequestInterface $request, ResponseInterface $response, $args)
+    {
+        $tab['logements'] = Logement::orderBy('moy','ASC')->take(6)->get();
+        $this->render($response,'home',$tab);
+    }
+}
